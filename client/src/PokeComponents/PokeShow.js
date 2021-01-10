f752006f2aafe25f26d00c3cd77b88d647905b56
Masa Takeuchi <masahitoart@gmail.com>
2021-01-10 16:53:33 +0000

    updated poke show

--- client/src/PokeComponents/PokeShow.js
+++ client/src/PokeComponents/PokeShow.js
@@ -6,6 +6,7 @@ function PokeShow() {
   const { id } = useParams()
   const [ hasError, setHasError ] = React.useState(false)
   const [ item, setItems ] = React.useState(null)
+  const [ itemQty, setItemQty ] = React.useState(null)
   
   React.useEffect(() => {
     const getData = async () => {
@@ -22,43 +23,73 @@ function PokeShow() {
 
   if (id) console.log('id',id)
 
-  if (item) {
-    console.log('item',item.avgRating)
-  } else {
-    console.log('no items')
+  function addToBasket(e){
+    e.preventDefault()
+    if (itemQty < 1) return
+    console.log(`add qty of ${itemQty} item id ${id} to the basket`)
   }
 
   function itemRating(n){
     console.log('n',n)
     const rating = []
     for (let i = 0; i < n; i++) rating.push('star') 
-    console.log('test',rating)
+    // console.log('test',rating)
     return rating
   }
 
-
+  function mapStars(rating){
+    let starId = 0
+    //* may need to call this outside function if this function get's used number of times
+    return rating.map(()=>{
+      starId ++
+      return (
+        <img key={starId} src="../assets/staryu.svg" alt="staryu" />
+      )
+    })
+  }
   
   return (
-    <div className="page_wrapper">
+    <div className="page_wrapper_column">
       { item ?
-        <div className="product_wrapper">
-          <img className= "product_image" src={item.image} alt={item.name} />
-          <div className="product_info">
-            <div className="rating">
-              rating {itemRating(item.avgRating).map((star)=>{
-                return (
-                  <img key={star} src="../assets/poke_dollar.svg" alt="pokedollar sign" />
-                )
-              })}
+        <>
+          <div className="product_wrapper">
+            <img className= "product_image" src={item.image} alt={item.name} />
+            <div className="product_info">
+              <div className="rating">
+                {mapStars(itemRating(item.avgRating))}
+              </div>
+              <p>{item.name}</p>
+              <p><img src="../assets/poke_dollar.svg" alt="pokedollar sign" />price {item.price}</p>
+              <p>description {item.description}</p>
+              <button>
+                <img src="../assets/pokeball_grey.svg" alt="add review" /> comment
+              </button>
             </div>
-            <p>{item.name}</p>
-            <p><img src="../assets/poke_dollar.svg" alt="pokedollar sign" />price {item.price}</p>
-            <p>description {item.description}</p>
-            <p>stock {item.stock}</p>
-            
+            <form className="buy_wrapper" onSubmit={addToBasket}>
+              {item.stock ?
+                <p>{item.stock} left in stock</p>
+                :
+                <p>sorry, out of stock</p>
+              }
+              <input type="number" name="qty" min="0" max={item.stock} onChange={(e)=>setItemQty(e.target.value)}/>
+              <button>
+                <img src="../assets/pokeball_grey.svg" alt="pokeball" /> add to basket
+              </button>
+            </form> 
           </div>
-        </div>
 
+          <div className="similar_items_wrapper">
+            similar items
+          </div>  
+
+          <div className="comments_wrapper">
+            comments
+
+            {/* <div className="comment_box">
+                comments
+            </div>   */}
+          </div>  
+        </>
         :
         hasError ? 
           <img src="../assets/pika_anim.gif" alt="loading" />
